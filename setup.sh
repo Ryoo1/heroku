950c1a914c1d94242493122b79de9fe7f35c3d74
Ryoo1 <74431039+Ryoo1@users.noreply.github.com>
2021-08-03 13:26:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,22 +6,22 @@ mkdir ./dev
 cd ./dev
 mkdir ./tmp
 cd ./tmp
-git clone "https://github.com/libDrive/server.git" --depth 1 server
+git clone "https://github.com/Ryoo1/server.git" --depth 1 server
 git clone "https://github.com/Ryoo1/web.git" --depth 1 web
-mkdir ./libDrive.Server
-mkdir ./libDrive.Server/build
-mkdir ./libDrive.Server/src
-mkdir ./libDrive.Server/templates
+mkdir ./Ryoo1.Server
+mkdir ./Ryoo1.Server/build
+mkdir ./Ryoo1.Server/src
+mkdir ./Ryoo1.Server/templates
 cd ./web
 yarn install
 yarn run build
-mv ./build/* ../libDrive.Server/build
+mv ./build/* ../Ryoo1.Server/build
 cd ../server
-mv main.py requirements.txt ../libDrive.Server
-mv ./src/* ../libDrive.Server/src
-mv ./templates/* ../libDrive.Server/templates
+mv main.py requirements.txt ../Ryoo1.Server
+mv ./src/* ../Ryoo1.Server/src
+mv ./templates/* ../Ryoo1.Server/templates
 cd ..
-cp ../../README.md ../../LICENSE ./libDrive.Server
-cd libDrive.Server
+cp ../../README.md ../../LICENSE ./Ryoo1.Server
+cd Ryoo1.Server
 mv ./* ../..
 cd ../../..
